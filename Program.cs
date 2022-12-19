6d58c8e3de3d45c4a0e82c8b765f846978c96b6c
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-19 23:03:39 +0300

    Task 1 done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+/*
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Input number: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+       
+    }
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+    Console.WriteLine();
+}
+
+int Count(int[]array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Input number of elements: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(length);
+ShowArray(myArray);
+int result = Count(myArray);
+
+Console.WriteLine($"The user input {result} values > 0");
+*/

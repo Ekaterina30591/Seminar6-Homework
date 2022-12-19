60cdd8921a8285059dfc6770b2585b35267c9466
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-19 23:05:35 +0300

    Task 2 done

--- Program.cs
+++ Program.cs
@@ -44,3 +44,24 @@ int result = Count(myArray);
 
 Console.WriteLine($"The user input {result} values > 0");
 */
+
+/*
+
+//Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Input b1 value: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k1 value: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b2 value: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k2 value: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1)/(k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Two lines will cross in point with X: {x} and Y: {y}");
+*/
